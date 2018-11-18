76ca33900bbe9f5383dbea2a83a0f42ed01e86ad
PikaFan123 <31591604+PikaFan123@users.noreply.github.com>
2018-11-18 14:09:25 +0100

    Update Program.cs

--- ChainBTPD/Program.cs
+++ ChainBTPD/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace ChainBTPD
 {
@@ -14,19 +10,22 @@ namespace ChainBTPD
             Console.WriteLine("Wenn du Pokemon chainst bekommst du Beeren und Bonbons");
             Console.WriteLine("Diese Dinge kannst du verkaufen! Du bekommst 10 P$ für ein Candy und 100 für eine Beere!");
             Console.WriteLine("So kannst du für die Kosten des Pokeballs aufkommen!");
-            Console.WriteLine("Das Programm geht von einer maximalen Chain von 1024 aus. Dann ist also Schluss! Lets Go!");
+            Console.WriteLine("Lets Go!");
             Console.WriteLine("Welches Pokemon chainst du?");
             var Mon = Console.ReadLine();
             long ChainBTPD = 0;
             long np;
             long np2;
-            Console.WriteLine("Wieviel kostet dein Fangball? (100=Pokeball, 300=Superball)");
+            Console.WriteLine("Wieviel kostet dein Fangball? (100=Pokeball, 300=Superball, 500=Hyperball)");
             int fangballkosten;
             fangballkosten = int.Parse(Console.ReadLine());
-            for (var i = 1; i <= 1024; i++)
+            Console.WriteLine("Wie lang wirst du chainen?");
+            long chainlength;
+            chainlength = long.Parse(Console.ReadLine());
+            for (var i = 1; i <= chainlength; i++)
             {
                 Console.WriteLine(Mon + " " + i);
-                Console.WriteLine("Wie viel Candy (1 Beere = 10 Candy) hast du bekommen?");
+                Console.WriteLine("Wie viel Candy (1 Beere = 10 Candy, einmal danebenwerfen bedeutet du musst Bonbons abziehen!(-10=Pokeball, -30=Superball, -50=Hyperball)) hast du bekommen?");
                 np = long.Parse(Console.ReadLine());
                 np2 = np * 10;
                 ChainBTPD = (ChainBTPD + np2) - fangballkosten;
